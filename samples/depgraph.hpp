33c63b85096f3a89482fd8d07d3171aa2e1a9968
Erik Saule <esaule@uncc.edu>
2017-12-25 18:13:37 -0500

    document api

--- samples/depgraph.hpp
+++ samples/depgraph.hpp
@@ -24,11 +24,15 @@ namespace depgraph {
   
   std::map<std::string, std::list<variableaccess> > accessmap;
   
+  ///declares a new task with a given name
+  ///@param name name of the task
   void newtask(const std::string& name) {
     tasklist.push_back(name);
     processing_time.push_back(1);
   }
 
+  
+  ///declares a new task. The name is the taskid 
   void newtask() {
     std::stringstream ss;
     ss<<tasklist.size()+1<<std::endl;
@@ -38,20 +42,25 @@ namespace depgraph {
   void setprocessingtime(int pi) {
     processing_time[processing_time.size()-1] = pi;
   }
-  
+
+  ///declares that the current task reads the given variable
+  ///@param name name of the variable read by the task
   void read(const std::string& name) {
     //assumes tasklist is not empty
     variableaccess va1 = {name, variableaccess::access::READ};
     accessmap[*(--(tasklist.end()))].push_back(va1);
   }
 
-
+  ///declares that the current task writes the given variable
+  ///@param name name of the variable writes by the task
   void write(const std::string& name) {
     //assumes tasklist is not empty
     variableaccess va2 = {name, variableaccess::access::WRITE}; 
     accessmap[*(--(tasklist.end()))].push_back(va2);
   }
-
+  
+  ///declares that the current task reads and writes the given variable
+  ///@param name name of the variable reads and writes by the task
   void readwrite(const std::string& name) {
     read(name);
     write(name);
